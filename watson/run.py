288353b36061325f9cc4e6ecfa203b9d5e90c19f
hemant10-1995 <31190062+hemant10-1995@users.noreply.github.com>
2018-02-04 20:37:27 +0530

    added keyword,categ
    Added extra parameter keyword, categorizes

--- watson/run.py
+++ watson/run.py
@@ -1,22 +1,26 @@
-import json
+
 from flask import jsonify
 from flask import Flask, render_template, request
 from watson_developer_cloud import NaturalLanguageUnderstandingV1
 from watson_developer_cloud.natural_language_understanding_v1 \
-import Features, EntitiesOptions, KeywordsOptions
+    import Features, EntitiesOptions, KeywordsOptions, RelationsOptions, SemanticRolesOptions, ConceptsOptions, \
+    CategoriesOptions
 
 app = Flask(__name__)
+
+
+natural_language_understanding = NaturalLanguageUnderstandingV1(
+  username='0f52f949-9645-408a-bbae-80d6f4512750',
+  password='TWUmeCwXnT1J',
+  version='2017-02-27')
 
 @app.route('/')
 def home():
  return render_template('input.html')
 
+
 @app.route('/result', methods=['POST'])
-def result():
-    natural_language_understanding = NaturalLanguageUnderstandingV1(
-        username='*****************',
-        password='*************',
-        version='2017-02-27')
+def resultjson():
 
     if request.method == 'POST':
         text1 = request.form
@@ -27,12 +31,22 @@ def result():
                 emotion=True,
                 sentiment=True,
                 limit=2),
+            relations=RelationsOptions(),
+            categories=CategoriesOptions(),
+            semantic_roles=SemanticRolesOptions(),
+            concepts=ConceptsOptions(
+                limit=3),
             keywords=KeywordsOptions(
                 emotion=True,
                 sentiment=True,
                 limit=2)))
         return jsonify(user)
-        #return render_template("result.html", result=user)
+
+
+
+
+
+
 
 if __name__ == '__main__':
  app.run(debug=True)
